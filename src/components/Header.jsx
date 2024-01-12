f4e1cda0bb134a8d3c8e83d373747f4c4dac8503
DarshitDhamecha <darshit.dhamecha07@gmail.com>
2024-01-12 15:02:46 +0530

    Changes in Header and create add some text and links in hero section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 export default function Header() {
-    return (
-        <>
-            <div className='text-white w-100 px-3 h-[65px] flex justify-between items-center header-bg sticky top-0'>
-                <div>
-                    <p className='fs-3'>Darshit Dhamecha</p>
-                </div>
-                <div className='flex gap-4'>
-                    <a href='#home' className='text- header-eff'>HOME</a>
-                    <a href='#about' className='text- header-eff'>ABOUT</a>
-                    <a href='#resume' className='text- header-eff'>RESUME</a>
-                    <a href='#' className='text- header-eff'>SKILLS</a>
-                    <a href='#' className='text- header-eff'>CONTECT</a>
-                </div>
-            </div>
-        </>
-    )
+
+  return (
+    <>
+      <div className='bg-[#3d4c53] h-[50px] border-b-2 border-[#797979] w-100 flex text-[#fff] items-center responsive'>
+        <a href='#' className=' header-eff'>HOME</a>
+        <a href='#' className=' header-eff'>RESUME</a>
+        <a href='#' className=' header-eff'>ABOUT</a>
+        <a href='#' className=' header-eff'>SKILLS</a>
+        <a href='#' className=' header-eff'>CONTECT</a>
+      </div>
+    </>
+  );
 }
