b93f1f704dcb94bec1d7672aa4a805b5844f4d52
DarshitDhamecha <darshit.dhamecha07@gmail.com>
2024-01-13 17:17:56 +0530

    change design of home page and also responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,9 @@ export default function Header() {
       <div className='bg-[#3d4c53] h-[50px] border-b-2 border-[#797979] w-100 flex text-[#fff] items-center responsive'>
         <a href='#' className=' header-eff'>HOME</a>
         <a href='#' className=' header-eff'>RESUME</a>
+        <a href='#' className=' header-eff'>PROJECTS</a>
         <a href='#' className=' header-eff'>ABOUT</a>
         <a href='#' className=' header-eff'>SKILLS</a>
-        <a href='#' className=' header-eff'>CONTECT</a>
       </div>
     </>
   );
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -3,43 +3,46 @@ import React from 'react';
 export default function Hero() {
     return (
         <>
-            <div className='w-100 flex items-center justify-center  bs-height  wrapper'>
-                <div className=' fram flex justify-center items-center border-anim rounded-[50%]'>
-                    <img src="/images/myPhoto-1.jpg" className='h-[230px] w-[230px] photo object-cover rounded-[50%]' alt="" />
+            <div className='w-100 bg-[#fffafa] bs-height'>
+                <div className='w-100 h-[45vh] flex justify-center items-center'>
+                    <div className='fram flex justify-center items-center border-anim rounded-[50%] mx-auto'>
+                        <img src="/images/myPhoto-1.jpg" className='photo object-cover rounded-[50%]' alt="" />
+                    </div>
                 </div>
-                <div className='text-[#3d4c53] flex'>
-                    <div className='mx-auto content'>
-                        <p className='font-semibold text-lg'>Hi, I'm Darshit Dhamecha</p>
-                        <p className='display-3 font-bold my-2'>Web Developer</p>
-                        <p className=''>Seeking a role as a front-end developer so I can use my thorough knowledge of programming frameworks and development software.</p>
-                        <div className='flex gap-3 mt-3 icons-align'>
-                            <a href='https://github.com/DarshitDhamecha'
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className='py-1 px-2 border-1 border-[#3d4c53] rounded-[50%] cursor-pointer hover:bg-[#3d4c53] hover:!text-[#fff] duration-200'
-                                title='View My Github'>
-                                <i class="fa-brands fa-github "></i>
-                            </a>
-                            <a href='#'
-                                title='View My LinkedIn'
-                                className='py-1 px-2 border-1 border-[#3d4c53] rounded-[50%] cursor-pointer hover:bg-[#3d4c53] hover:!text-[#fff] duration-200'>
-                                <i class="fa-brands fa-linkedin-in "></i>
-                            </a>
-                            <a href='tel:7048416907'
-                                target='_blank'
-                                rel='noopener noreferrer'
-                                title='Contect Me'
-                                className='py-1 px-2 border-1 border-[#3d4c53] rounded-[50%] cursor-pointer hover:bg-[#3d4c53] hover:!text-[#fff] duration-200'>
-                                <i class="fa-brands fa-whatsapp"></i>
-                            </a>
-                            <a href='https://www.instagram.com/darshit.444'
-                                target='_blank'
-                                rel='noopener noreferrer'
-                                title='View My Instagram'
-                                className='py-1 px-2 border-1 border-[#3d4c53] rounded-[50%] cursor-pointer hover:bg-[#3d4c53] hover:!text-[#fff] duration-200'>
-                                <i class="fa-brands fa-instagram"></i>
-                            </a>
-                        </div>
+                <div className='text-[#3d4c53] w-100 justify-center mt-3 text-center w-100 px-2'>
+                    <p className='font-semibold text-lg '>Hi, I'm Darshit Dhamecha</p>
+                    <div className="w-100">
+                        <div className='h-[2px] w-[150px] bg-[#3d4c53] mx-auto my-2'></div>
+                    </div>
+                    <p className='display-3 font-bold'>Web Developer</p>
+                    <p className='text mt-3'>Seeking a role as a front-end developer so I can use my thorough knowledge of programming frameworks and development software.</p>
+                    <div className='flex gap-3 mt-4 icons-align justify-center w-100'>
+                        <a href='https://github.com/DarshitDhamecha'
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className='py-1 px-2 border-1 border-[#3d4c53] rounded-[50%] cursor-pointer hover:bg-[#3d4c53] hover:!text-[#fff] duration-200'
+                            title='View My Github'>
+                            <i class="fa-brands fa-github "></i>
+                        </a>
+                        <a href='#'
+                            title='View My LinkedIn'
+                            className='py-1 px-2 border-1 border-[#3d4c53] rounded-[50%] cursor-pointer hover:bg-[#3d4c53] hover:!text-[#fff] duration-200'>
+                            <i class="fa-brands fa-linkedin-in "></i>
+                        </a>
+                        <a href='tel:7048416907'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            title='Contect Me'
+                            className='py-1 px-2 border-1 border-[#3d4c53] rounded-[50%] cursor-pointer hover:bg-[#3d4c53] hover:!text-[#fff] duration-200'>
+                            <i class="fa-brands fa-whatsapp"></i>
+                        </a>
+                        <a href='https://www.instagram.com/darshit.444'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            title='View My Instagram'
+                            className='py-1 px-2 border-1 border-[#3d4c53] rounded-[50%] cursor-pointer hover:bg-[#3d4c53] hover:!text-[#fff] duration-200'>
+                            <i class="fa-brands fa-instagram"></i>
+                        </a>
                     </div>
                 </div>
             </div>
