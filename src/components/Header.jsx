b817b06ea8f1deccb1e6ecd14e5fc73eb3ea469e
DarshitDhamecha <darshit.dhamecha07@gmail.com>
2024-01-26 12:47:24 +0530

    change resume page and header is going on.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import "../../src/assets/header.css"
+import "../../src/assets/header.css";
 
 export default function Header() {
+  // Use state to manage the class for each link separately
+  const [homeClass, setHomeClass] = useState(false);
+  const [resumeClass, setResumeClass] = useState(false);
+  const [projectsClass, setProjectsClass] = useState(false);
+  const [aboutClass, setAboutClass] = useState(false);
+  const [skillsClass, setSkillsClass] = useState(false);
+
+  const handleHomeClick = () => {
+    setHomeClass(!homeClass);
+  };
+
+  const handleResumeClick = () => {
+    setResumeClass(!resumeClass);
+  };
+
+  const handleProjectsClick = () => {
+    setProjectsClass(!projectsClass);
+  };
+
+  const handleAboutClick = () => {
+    setAboutClass(!aboutClass);
+  };
+
+  const handleSkillsClick = () => {
+    setSkillsClass(!skillsClass);
+  };
 
   return (
     <>
       <div className='bg-[#3d4c53] sticky top-0 z-50 h-[50px] border-b-2 border-[#797979] w-100 flex text-[#fff] items-center responsive'>
-        <Link to='/' className='header-eff'>HOME</Link>
-        <Link to='/resume' className='header-eff'>RESUME</Link>
-        <Link to='#' className='header-eff'>PROJECTS</Link>
-        <Link to='/about' className='header-eff'>ABOUT</Link>
-        <Link to='/skills' className='header-eff'>SKILLS</Link>
+        <Link to='/' className={`header-eff ${homeClass ? 'addclass' : ''}`} onClick={handleHomeClick}>HOME</Link>
+        <Link to='/resume' className={`header-eff ${resumeClass ? 'addclass' : ''}`} onClick={handleResumeClick}>RESUME</Link>
+        <Link to='#' className={`header-eff ${projectsClass ? 'addclass' : ''}`} onClick={handleProjectsClick}>PROJECTS</Link>
+        <Link to='/about' className={`header-eff ${aboutClass ? 'addclass' : ''}`} onClick={handleAboutClick}>ABOUT</Link>
+        <Link to='/skills' className={`header-eff ${skillsClass ? 'addclass' : ''}`} onClick={handleSkillsClick}>SKILLS</Link>
       </div>
     </>
   );
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,11 +4,13 @@ import "../../src/assets/resume.css"
 export default function Resume() {
     return (
         <>
-            {/* <img src="/images/resume.png" className='my-height w-100 object-cover' alt="" /> */}
-            <div className='resume-back px-2'>
-                <div className='my-resume'>
-
-                </div>
+            <div className='height px-2'>
+                <img src="/images/myresume.jpg" className='h-100 mx-auto mt-3 resume-shadow' alt="" />
+            </div>
+            <div className='position-fixed bottom-7 right-7'>
+                <a href="/images/myresume.jpg" download="Darshit_Dhamecha_CV">
+                    <button className='border-[#3d4c53] border-1 py-1 px-3 text- hover:bg-      [#3d4c53] hover:text-[#fff]'>Download</button>
+                </a>
             </div>
         </>
     )
