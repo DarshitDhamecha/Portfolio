96e295e84efc24bb09b281f7dd7213d11f9071dd
DarshitDhamecha <darshit.dhamecha07@gmail.com>
2024-01-28 19:36:58 +0530

    add animations

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import "../../src/assets/header.css";
+import Aos from 'aos';
 
 export default function Header() {
 
+  useEffect(() => {
+    Aos.init({
+      duration: 1500,
+    })
+  },[])
+
   return (
     <>
-      <div className='bg-[#3d4c53] sticky top-0 z-50 h-[50px] border-b-2 border-[#797979] w-100 flex text-[#fff] items-center responsive'>
+      <div className='bg-[#3d4c53] sticky top-0 z-50 h-[50px] border-b-2 border-[#797979] w-100 flex text-[#fff] items-center responsive' data-aos="fade-down">
         <Link to='/' className='header-eff' >HOME</Link>
         <Link to='/resume' className='header-eff' >RESUME</Link>
         <Link to='/project' className='header-eff' >PROJECTS</Link>
